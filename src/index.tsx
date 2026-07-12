b7b76a387dddccadc97c24642ba2b8241c238b8a
Joseph David Gari Bustos <josephgari@gmail.com>
2026-07-12 13:46:30 -0500

    feat: route standalone Ollama setup through React

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import OllamaSetupStandalone from './components/OllamaSetupStandalone';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
+
+const params = new URLSearchParams(window.location.search);
+const isOllamaSetupView = params.get('view') === 'ollama-setup';
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {isOllamaSetupView ? <OllamaSetupStandalone /> : <App />}
+  </React.StrictMode>,
 );
